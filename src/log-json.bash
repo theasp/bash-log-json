a698e7b392133f35dbe3979fc959e50089ca6dc1
Andrew Phillips <andrew.phillips2@canada.ca>
2018-05-17 16:25:45 -0300

    Fall back to perl to encode JSON

--- src/log-json.bash
+++ src/log-json.bash
@@ -1,18 +1,34 @@
-function __log_json__session_init {
-  if [[ $__LOG_JSON__SESSION_INIT != true ]]; then
-    __LOG_JSON__HOSTNAME=$(hostname -f)
-    __LOG_JSON__ID=0
-    __LOG_JSON__FINISHED=0
-    __LOG_JSON__TTY=$(tty)
-    __LOG_JSON__SESSION=$(uuidgen)
-    __LOG_JSON__LOG_DIR=~/.bash_log/${__LOG_JSON__HOSTNAME}/
-    __LOG_JSON__FILE=${__LOG_JSON__LOG_DIR}/${__LOG_JSON__SESSION}.json
+function __log_json__get_engine {
+  if which jq 2>&1 > /dev/null; then
+    echo jq
+  elif (which perl; perl -MJSON::PP -e true) >/dev/null 2>&1; then
+    echo perl
+  fi
+}
 
-    (umask 0077; mkdir -p ${__LOG_JSON__LOG_DIR})
-    precmd_functions+=(__log_json__precmd)
-    preexec_functions+=(__log_json__preexec)
-    __log_json__session_event_jq >> $__LOG_JSON__FILE
-    __LOG_JSON__SESSION_INIT=true
+function __log_json__session_init {
+  __LOG_JSON__ENGINE=$(__log_json__get_engine)
+  if [[ $__LOG_JSON__ENGINE ]]; then
+    if [[ $__LOG_JSON__SESSION_INIT != true ]]; then
+      __LOG_JSON__HOSTNAME=$(hostname -f)
+      __LOG_JSON__ID=0
+      __LOG_JSON__FINISHED=0
+      __LOG_JSON__TTY=$(tty)
+      __LOG_JSON__SESSION=$(uuidgen)
+      __LOG_JSON__LOG_DIR=~/.bash_log/${__LOG_JSON__HOSTNAME}/
+      __LOG_JSON__FILE=${__LOG_JSON__LOG_DIR}/${__LOG_JSON__SESSION}.json
+      __LOG_JSON__SESSION_INIT=true
+
+      (umask 0077; mkdir -p ${__LOG_JSON__LOG_DIR})
+      case $__LOG_JSON__ENGINE in
+        jq)   __log_json__session_event_jq >> $__LOG_JSON__FILE ;;
+        perl) __log_json__session_event_perl >> $__LOG_JSON__FILE ;;
+      esac
+      precmd_functions+=(__log_json__precmd)
+      preexec_functions+=(__log_json__preexec)
+    fi
+  else
+    echo "Unable to intialize log-json, need jq or perl installed" 1>&2
   fi
 }
 
@@ -27,9 +43,31 @@ function __log_json__session_event_jq {
        "time": now,
        "session": $session,
        "hostname": $hostname,
-       "pid": $pid,
+       "pid": $pid|fromjson,
        "tty": $tty,
-       "level": $level}'
+       "level": $level|fromjson}'
+}
+
+function __log_json__session_event_perl {
+  perl - \
+       "$__LOG_JSON__SESSION" \
+       "$__LOG_JSON__HOSTNAME" \
+       "$$" \
+       "$__LOG_JSON__TTY" \
+       "$SHLVL" <<-'EOF'
+use Time::HiRes qw(time);
+use JSON::PP;
+($session, $hostname, $pid, $tty, $level) = @ARGV;
+$data = {type => session,
+         time => time(),
+         session => $session,
+         hostname => $hostname,
+         pid => int($pid),
+         tty => $tty,
+         level => int($level)};
+
+print(encode_json($data) . "\n");
+EOF
 }
 
 function __log_json__start_event_jq {
@@ -42,8 +80,28 @@ function __log_json__start_event_jq {
        "time": now,
        "session": $session,
        "hostname": $hostname,
-       "id": $id,
+       "id": $id|fromjson,
        "cmd": $cmd}'
+}
+
+function __log_json__start_event_perl {
+  perl -\
+       "$__LOG_JSON__SESSION" \
+       "$__LOG_JSON__HOSTNAME" \
+       "$__LOG_JSON__ID" \
+       "$__LOG_JSON__CMD" <<-'EOF'
+use Time::HiRes qw(time);
+use JSON::PP;
+($session, $hostname, $id, $cmd) = @ARGV;
+$data = {type => session,
+         time => time(),
+         session => $session,
+         hostname => $hostname,
+         id => int($id),
+         cmd => $cmd};
+
+print(encode_json($data) . "\n")
+EOF
 }
 
 function __log_json__end_event_jq {
@@ -57,24 +115,64 @@ function __log_json__end_event_jq {
        "time": now,
        "session": $session,
        "hostname": $hostname,
-       "id": $id,
+       "id": $id|fromjson,
        "cmd": $cmd,
-       "rc": $rc}' >> $__LOG_JSON__FILE
+       "rc": $rc|fromjson}' >> $__LOG_JSON__FILE
 }
 
+function __log_json__end_event_perl {
+  perl - \
+       "$__LOG_JSON__SESSION" \
+       "$__LOG_JSON__HOSTNAME" \
+       "$__LOG_JSON__ID" \
+       "$__LOG_JSON__CMD" \
+       "$__LOG_JSON__RC" <<-'EOF'
+use Time::HiRes qw(time);
+use JSON::PP;
+($session, $hostname, $id, $cmd, $rc) = @ARGV;
+$data = {type => session,
+         time => time(),
+         session => $session,
+         hostname => $hostname,
+         id => int($id),
+         cmd => $cmd};
+
+print(encode_json($data) . "\n")
+EOF
+}
 
 function __log_json__preexec {
   __LOG_JSON__CMD="$1"
   __LOG_JSON__ID=$(( __LOG_JSON__ID + 1 ))
-  __log_json__start_event_jq >> $__LOG_JSON__FILE
+  case $__LOG_JSON__ENGINE in
+    jq)   __log_json__start_event_jq >> $__LOG_JSON__FILE ;;
+    perl) __log_json__start_event_perl >> $__LOG_JSON__FILE ;;
+  esac
 }
 
 function __log_json__precmd {
   __LOG_JSON__RC=$?
   if [[ $__LOG_JSON__FINISHED != $__LOG_JSON__ID ]]; then
     __LOG_JSON__FINISHED=$__LOG_JSON__ID
-    __log_json__end_event_jq >> $__LOG_JSON__FILE
+    case $__LOG_JSON__ENGINE in
+      jq)   __log_json__end_event_jq >> $__LOG_JSON__FILE ;;
+      perl) __log_json__end_event_perl >> $__LOG_JSON__FILE ;;
+    esac
   fi
 }
 
 __log_json__session_init
+
+# echo "Perl"
+# time {
+#   for i in $(seq 1000); do
+#     __log_json__session_event_perl > /dev/null
+#   done
+# }
+
+# echo "JQ"
+# time {
+#   for i in $(seq 1000); do
+#     __log_json__session_event_jq > /dev/null
+#   done
+# }
